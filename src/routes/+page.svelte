9382c0ecce8afcd98871c39cb88fba6a5f6b0712
Glo04-svg <gloria.vergani@mail.polimi.it>
2026-03-25 17:28:42 +0100

    add state and doubled

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,22 @@
 <script>
- console.log('Hello from SvelteKit!');
+ console.log('Hello from SvelteKit!');   
+ let count = $state(2);
+ let doubled = $derived(count * 2);
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button on:click={() => count++}>
+    clicks: {count}
+</button>
+
+<span>
+    doubled: {doubled}
+</span>
+
 <style>
     h1 {
         color: deepskyblue;
     }
-    
 </style>
