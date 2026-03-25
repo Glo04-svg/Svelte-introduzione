70495d7e8715379d978f2828afa7a1c8dc13043a
Glo04-svg <gloria.vergani@mail.polimi.it>
2026-03-25 17:40:47 +0100

    cambiamenti

---
+++ src/lib/components/ClicksCounter.svelte
@@ -0,0 +1,15 @@
+<script>
+ console.log('Hello from SvelteKit!');   
+ let count = $state(2);
+ let doubled = $derived(count * 2);
+</script>
+
+<h3>Clicks Counter</h3>
+
+<button on:click={() => count++}>
+    clicks: {count}
+</button>
+
+<span>
+    doubled: {doubled}
+</span>
