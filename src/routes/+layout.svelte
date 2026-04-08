ea831cf645d7479bf3aed6f79e3d3a0515d0201c
Glo04-svg <gloria.vergani@mail.polimi.it>
2026-04-08 22:39:59 +0200

    cambiamenti stilistici

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,58 +1,50 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
-	let { children } = $props();
+  import favicon from "$lib/assets/favicon.svg";
+  let { children } = $props();
 </script>
 
-
 <svelte:head>
-	<link rel="icon" href={favicon} />
-	<!-- Import font pixelato da Google Fonts -->
-	<link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+  <link rel="icon" href={favicon} />
+  <!-- Import font pixelato da Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
+    rel="stylesheet"
+  />
 </svelte:head>
 
 {@render children()}
 
-
-
-<img src=https://media4.giphy.com/media/v1.Y2lkPTZjMDliOTUyNHlhNjQzc3h2dWs2ZTc5OHJ4czJpcDF4bGxnN3k2b3ljY3U5ZHdocCZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/10a8AOSeP6Rqfu/giphy.gif alt="Cute cat gif" />
-
 <footer>
-	<p>© Polimi 2026</p>
+  <p>© Polimi 2026</p>
 </footer>
 
 <style>
+  :global(p) {
+    color: #ff4ebe;
+    font-family: "Press Start 2P", cursive;
+  }
 
+  footer {
+    text-align: center;
+    padding: 10px 30px 15px 30px;
+    width: 100%;
+    background-color: #f0f0f0;
+    margin-top: auto;
+    height: 10vh;
+  }
 
-      :global(p) {
-        color: #ff4ebe;
-		font-family: 'Press Start 2P', cursive;
+  :global(body) {
+    background-color: #1b1818;
+    color: #fff;
+    margin: auto;
+    font-family: "Press Start 2P", cursive;
+  }
 
-    }
-	
-	footer {
-		text-align: center;
-		padding: 10px 30px 15px 30px;
-		width: 100%;
-		background-color: #f0f0f0;
-	}
-
-:global(body) {
-        background-color: #1b1818; 
-        color: #fff; 
-        margin: auto; 
-        font-family: 'Press Start 2P', cursive;
-    }
-
-	img {
-		display: block;
-		margin: 20px auto;
-		max-width: 100%;
-		height: auto;
-		border-radius: 10px;
-		
-	}
-
-	
-	
-	
-	</style>
+  img {
+    display: block;
+    margin: 20px auto;
+    max-width: 100%;
+    height: auto;
+    border-radius: 10px;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,61 @@
 <script>
-  import { website } from '$lib/state.svelte.js';
-import ClicksCounter from '$lib/components/ClicksCounter.svelte';
-import CanvasSquare from '$lib/components/CanvasSquare.svelte';
-  import TodoList from '$lib/components/TodoList.svelte';
-  import TitleEditor from '$lib/components/TitleEditor.svelte';
+  import { website } from "$lib/state.svelte.js";
+  import ClicksCounter from "$lib/components/ClicksCounter.svelte";
+  import CanvasSquare from "$lib/components/CanvasSquare.svelte";
+  import TodoList from "$lib/components/TodoList.svelte";
+  import TitleEditor from "$lib/components/TitleEditor.svelte";
 
-let count = $state(5);
-console.log('Ciao MERDE!');
+  let count = $state(5);
+  console.log("Ciao MERDE!");
+</script>
 
-</script> 
+<div class="flex-content">
+  <div class="centered-content">
+    <h1>{website.title}</h1>
+    <p>
+      Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read
+      the documentation
+    </p>
 
+    <TitleEditor />
 
+    <ClicksCounter bind:initialCount={count} />
 
-<h1>{website.title}</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+    {#if count > 10 && count < 20}
+      <p>count from parent is {count}</p>
+    {:else if count > 30}
+      <p>that's a lot of clicks!</p>
+    {:else}
+      <p>count from parent is not in range</p>
+    {/if}
 
-<TitleEditor />
+    <CanvasSquare />
 
-<ClicksCounter bind:initialCount={count} />
+    <TodoList />
+  </div>
+  <img
+    src="https://media4.giphy.com/media/v1.Y2lkPTZjMDliOTUyNHlhNjQzc3h2dWs2ZTc5OHJ4czJpcDF4bGxnN3k2b3ljY3U5ZHdocCZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/10a8AOSeP6Rqfu/giphy.gif"
+    alt="Cute cat gif"
+  />
+</div>
 
+<style>
+  h1 {
+    color: rgb(255, 230, 251);
+    font-family: "Press Start 2P", cursive;
+  }
 
-{#if count > 10 && count < 20}
-     <p> count from parent is {count} </p>
-{:else if count > 30}
-     <p> that's a lot of clicks! </p>
-{:else}
-     <p> count from parent is not in range </p>
-{/if}
- 
-<CanvasSquare />
+  .flex-content {
+    display: flex;
+    gap: 2em;
+    align-items: center;
+    justify-content: center;
+    height: 90vh;
+  }
 
-<TodoList />
+  .centered-content {
+    margin: 0 auto;
 
-
-
-<style> 
-
-    h1 {
-        color: rgb(255, 230, 251);
-        font-family: 'Press Start 2P', cursive;
-    }
-  
+    max-width: fit-content;
+  }
 </style>
